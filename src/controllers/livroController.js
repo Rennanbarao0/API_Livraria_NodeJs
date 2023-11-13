c940d82d85243d7d13e9777793df470748e2b5e6
Rennanbarao0 <rennanbezerra07@gmail.com>
2023-11-13 17:55:31 -0300

    refactor: Method and route to delete book by ID

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -47,6 +47,18 @@ class LivroController {
       });
     }
   }
+
+  static async deletarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(200).json({ message: "Livro deletado com sucesso!" });
+    } catch (error) {
+      res.status(500).json({
+        message: `${erro.message} - Falha ao tentar deletar livro!`,
+      });
+    }
+  }
 }
 
 export default LivroController;
