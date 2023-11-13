278f4089b0b98977e4f27255a1a729dc7ac67d8c
Rennanbarao0 <rennanbezerra07@gmail.com>
2023-11-13 17:39:51 -0300

    refactor: Creation of book registration in the Controller

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -11,6 +11,18 @@ class LivroController {
       });
     }
   }
+  static async cadastrarLivro(req, res) {
+    try {
+      const novoLivro = await livro.create(req.body);
+      res
+        .status(201)
+        .json({ message: "Criado com sucesso!", livro: novoLivro });
+    } catch (erro) {
+      res.status(500).json({
+        message: `${erro.message} - Não foi possível cadastrar o livro!`,
+      });
+    }
+  }
 }
 
 export default LivroController;
