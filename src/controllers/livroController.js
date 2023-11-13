034109decbffb1f3eea64f7bf750b08979a8e0a6
Rennanbarao0 <rennanbezerra07@gmail.com>
2023-11-13 17:38:56 -0300

    refactor: Creation of a list of all books in the GET route

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,8 +2,14 @@ import livro from "../models/Livro.js";
 
 class LivroController {
   static async listarLivros(req, res) {
-    const listaLivros = await livro.find({});
-    res.status(200).json(listaLivros);
+    try {
+      const listaLivros = await livro.find({});
+      res.status(200).json(listaLivros);
+    } catch (error) {
+      res.status(500).json({
+        message: `${erro.message} - Falha ao tentar encontrar lista de livros!`,
+      });
+    }
   }
 }
 
