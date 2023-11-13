16ce7fce78fe568b542d1fcb3f206161f79cd236
Rennanbarao0 <rennanbezerra07@gmail.com>
2023-11-13 17:49:44 -0300

    refactor: Routes and methods updated in the code

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -31,7 +31,19 @@ class LivroController {
       res.status(200).json(livroEncontradoPorId);
     } catch (error) {
       res.status(500).json({
-        message: `${erro.message} - Falha ao tentar encontrar livro!`,
+        message: `${erro.message} - Falha ao tentar listar livro!`,
+      });
+    }
+  }
+
+  static async atualizarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "Livro atualizado com sucesso!" });
+    } catch (error) {
+      res.status(500).json({
+        message: `${erro.message} - Falha ao tentar atualizar livro!`,
       });
     }
   }
