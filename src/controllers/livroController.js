6c4bfd117aff32b68f535bbce1a05f7386db94ed
Rennanbarao0 <rennanbezerra07@gmail.com>
2023-11-14 13:40:19 -0300

    feat: Creation of a method to list publishers

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -66,6 +66,18 @@ class LivroController {
       });
     }
   }
+
+  static async listarLivrosPorEditora(req, res) {
+    const editora = req.query.editora;
+    try {
+      const livrosPorEditora = await livro.find({ editora: editora });
+      res.status(200).json(livrosPorEditora);
+    } catch (error) {
+      res.status(500).json({
+        message: `${error.message} - Não foi possível localizar a editora!`,
+      });
+    }
+  }
 }
 
 export default LivroController;
